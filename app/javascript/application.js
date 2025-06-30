67f06a4d8bf167a87697c4d78e40ba6e2f527ef3
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-06-30 19:42:35 +0900

    esbuild導入の設定

---
+++ app/javascript/application.js
@@ -0,0 +1,16 @@
+// Entry point for the build script in your package.json
+// app/javascript/application.js
+
+// Hotwire Turbo (Rails newでデフォルトで入っています)
+import "@hotwired/turbo-rails"
+
+// Stimulus (Rails newでデフォルトで入っています)
+import "./controllers" // もしStimulusを使うなら。デフォルトでこの行があります。
+
+// BootstrapのJavaScriptをインポート (Popper.jsが先)
+import * as Popper from '@popperjs/core'
+window.Popper = Popper // BootstrapがグローバルなPopperを期待する場合に必要
+import 'bootstrap'
+
+// その他のカスタムJavaScriptファイルもここからインポートできます
+// 例: import "./my_custom_script"
