b95817c59f86cd582cf94709d8de0442ae335a5b
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-07-04 01:23:44 +0900

    hotwire: false

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,10 @@
 // app/javascript/application.js
 
 // Hotwire Turbo (Rails newでデフォルトで入っています)
-import "@hotwired/turbo-rails"
+// import "@hotwired/turbo-rails"
 
 // Stimulus (Rails newでデフォルトで入っています)
-import "./controllers" // もしStimulusを使うなら。デフォルトでこの行があります。
+// import "./controllers" // もしStimulusを使うなら。デフォルトでこの行があります。
 
 // BootstrapのJavaScriptをインポート (Popper.jsが先)
 import * as Popper from '@popperjs/core'
