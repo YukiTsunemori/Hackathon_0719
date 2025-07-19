95cce847fab32b1569214317e8e3808619317a7f
Sone Tatsuki <futenma3312@gmail.com>
2025-07-19 17:16:00 +0900

    ログイン・ログアウト・新規登録機能の追加）

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,2 @@
-// Entry point for the build script in your package.json
-// app/javascript/application.js
-
-// Hotwire Turbo (Rails newでデフォルトで入っています)
-// import "@hotwired/turbo-rails"
-
-// Stimulus (Rails newでデフォルトで入っています)
-// import "./controllers" // もしStimulusを使うなら。デフォルトでこの行があります。
-
-// BootstrapのJavaScriptをインポート (Popper.jsが先)
-import * as Popper from '@popperjs/core'
-window.Popper = Popper // BootstrapがグローバルなPopperを期待する場合に必要
-import 'bootstrap'
-
-// その他のカスタムJavaScriptファイルもここからインポートできます
-// 例: import "./my_custom_script"
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root "pages#test_bootstrap"
+  root "posts#index"
+  get "posts/index"
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
+  resource :posts
+  # get "/users/sign_out", to: "devise/sessions#destroy"
 end
